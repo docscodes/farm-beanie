1276bbe3b4762416b46d237d82b93716f8df613c
docscodes <docscodes@gmail.com>
2025-05-31 12:00:47 -0400

    add title

--- main.py
+++ main.py
@@ -5,17 +5,19 @@ from database import init_db
 from routers import user as user_router
 from routers import cars as cars_router
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await init_db()
     yield
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(lifespan=lifespan, title='farm-beanie')
 CORS(app)
 
 app.include_router(user_router.router, prefix="/users", tags=["Users"])
 app.include_router(cars_router.router, prefix="/cars", tags=["Cars"])
+
 
 @app.get("/", tags=["Root"])
 async def read_root() -> dict:
